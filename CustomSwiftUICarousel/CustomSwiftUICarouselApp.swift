e97af64f2f021cedb389335cefe561a0905c4df0
Andras Pal <andr4s.pal@gmail.com>
2021-01-14 16:16:17 +0000

    Initial Commit

---
+++ CustomSwiftUICarousel/CustomSwiftUICarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUICarouselApp.swift
+//  CustomSwiftUICarousel
+//
+//  Created by Andras Pal on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUICarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
