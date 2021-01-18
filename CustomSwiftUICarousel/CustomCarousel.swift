56ddb71fa49d54ed74e73a97ba448da67d52ef9f
Andras Pal <andr4s.pal@gmail.com>
2021-01-18 19:01:20 +0000

    Add Enums.swift, remove the enum from CustomCarousel.swift

--- CustomSwiftUICarousel/CustomCarousel.swift
+++ CustomSwiftUICarousel/CustomCarousel.swift
@@ -195,23 +195,6 @@ private struct CarouselCell : View {
     }
 }
 
-//=======================================
-// MARK: Enums
-//=======================================
-enum DragState {
-    
-    case inactive
-    case dragging(translation: CGSize)
-    
-    var translation: CGSize {
-        
-        if case let .dragging(translation) = self {
-            return translation
-        }
-        return .zero
-    }
-}
-
 
 //=======================================
 // MARK: Previews
---
+++ CustomSwiftUICarousel/Enums.swift
@@ -0,0 +1,16 @@
+
+import SwiftUI
+
+enum DragState {
+    
+    case inactive
+    case dragging(translation: CGSize)
+    
+    var translation: CGSize {
+        
+        if case let .dragging(translation) = self {
+            return translation
+        }
+        return .zero
+    }
+}
